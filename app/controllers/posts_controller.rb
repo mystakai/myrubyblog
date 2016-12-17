ef65235fdfe3f1765b5e6b9f9276ce3864980074
Tobias <whisperingwyrd@gmail.com>
2016-12-16 21:40:31 -0500

    category and post database created; posts controller updated

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+  def index
+    @content_first = 'This is the First Content!!!'
+    @content_second = 'This is the Second Content!!!'
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
