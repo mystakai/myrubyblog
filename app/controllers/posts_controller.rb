8084cbc3d3d84a42a8c369ce41079b655b7d325b
Tobias <whisperingwyrd@gmail.com>
2016-12-18 21:58:22 -0500

    basic CRUD added for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,52 @@
 class PostsController < ApplicationController
 
   def index
-    @content_first = 'This is the First Content!!!'
-    @content_second = 'This is the Second Content!!!'
+    @posts = Post.all
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, {notice: 'Your post was saved'}
+    else
+      @category = Category.all
+      render :new
+    end
   end
 
   def new
+    @post = Post.new
+    @category = Category.all
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @category = Category.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to posts_path, {notice: 'Update Success!'}
+    else
+      @category = Category.all
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, {notice: 'Post Destryed.'}
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
   end
 
 end
